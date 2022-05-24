f28a3003570b19b5b11875f6ffc84a3ec3c44264
kihno <stistyle13@gmail.com>
2022-05-24 13:00:12 -0700

    Add weather icons to weather display

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import {weather} from '../src/weather';
 import {slider} from '../src/slider.js';
 
-// window.onload = weather.getWeather('los angeles');
+window.onload = weather.getWeather('los angeles');
--- src/weather.js
+++ src/weather.js
@@ -1,4 +1,12 @@
 import {slider} from './slider.js';
+import Clear from '../images/clear-day.svg';
+import Clouds from '../images/clouds.svg';
+import Drizzle from '../images/drizzle.svg';
+import Rain from '../images/rain.svg';
+import Thunderstorm from '../images/thunderstorm.svg';
+import Snow from '../images/snow.svg';
+import Fog from '../images/fog.svg';
+import Tornado from '../images/tornado.svg';
 
 export const weather = (() => {
 
@@ -11,17 +19,19 @@ export const weather = (() => {
     let weatherData = {};
 
     async function getWeather(location) {
-        let city = input.value;
+        let city = input.value || location;
+        
         let response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=256f2fb92e7b31de1dd3240fad6d6f0e', {mode: 'cors'});
         let data = await response.json();
 
+        
         weatherData.city = data.name;
         weatherData.temp = Math.round(data.main.temp);
         weatherData.condition = data.weather[0].main;
         weatherData.low = Math.round(data.main.temp_min);
         weatherData.high = Math.round(data.main.temp_max)
         weatherData.humidity = Math.round(data.main.humidity);
-  
+        console.log(weatherData);
         clear();
         renderWeather();
     }
@@ -33,6 +43,12 @@ export const weather = (() => {
         if (prop === 'temp' || prop === 'low' || prop === 'high') {
             div.classList.add('far');
             div.textContent = data;
+        } else if (prop === 'condition') {
+            div.appendChild(appendIcon(data));
+            console.log(appendIcon(data));
+            let subDiv = document.createElement('div');
+            subDiv.textContent = data;
+            div.appendChild(subDiv);
         } else {
             div.textContent = data;
         }
@@ -48,6 +64,36 @@ export const weather = (() => {
         }
     }
 
+    function appendIcon(data) {
+        const icon = new Image();
+        
+        if (data === 'Clear') {
+            icon.src = Clear;
+            return icon;
+        } else if (data === 'Clouds') {
+            icon.src = Clouds;
+            return icon;
+        } else if (data === 'Rain') {
+            icon.src = Rain;
+            return icon;
+        } else if (data === 'Drizzle') {
+            icon.src = Drizzle;
+            return icon;
+        } else if (data === 'Snow') {
+            icon.src = Snow;
+            return icon;
+        } else if (data === 'Thunderstrom') {
+            icon.src = Thunderstorm;
+            return icon;
+        } else if (data === 'Tornado') {
+            icon.src = Tornado;
+            return icon;
+        } else {
+            icon.src = Fog;
+            return icon;
+        }
+    }
+
     function renderWeather() {
         for (let prop in weatherData) {
             let value = weatherData[prop];
@@ -56,5 +102,4 @@ export const weather = (() => {
     }
 
     return {getWeather}
-    // window.onload = getWeather('los angeles');
 })();

2966fb75c3faec4e1815a44fd84b02a3efd117c9
kihno <stistyle13@gmail.com>
2022-06-17 16:11:36 -0700

    Fix broken links

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import css from '../src/stylesheet.css';
+import '../src/stylesheet.css';
 import Logo from '../images/tempcheck-white.png';
 import {weather} from '../src/weather';
 import {slider} from '../src/slider.js';
+
+const logo = document.getElementById('logo');
+logo.src = Logo;
 
 window.onload = weather.getWeather('los angeles');

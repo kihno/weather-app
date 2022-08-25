b6e7116ceab1851ef665fe98e1a40ebad9506ef4
kihno <stistyle13@gmail.com>
2022-08-25 11:24:55 -0700

    Upload logo image

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import {weather} from '../src/weather';
 import {slider} from '../src/slider.js';
 
 const logo = document.getElementById('logo');
-logo.src = Logo;
 
+function loadImages() {
+    logo.src = Logo;
+}
+
+loadImages();
 window.onload = weather.getWeather('los angeles');
